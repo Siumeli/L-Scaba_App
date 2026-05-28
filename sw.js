f9b8737684b3429aed8f074221c8b058f14c28d5
Siumeli <samelisamppu@gmail.com>
2026-05-28 07:42:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -33,6 +33,7 @@ self.addEventListener('activate', (e) => {
   )));
 });
 
-self.addEventListener('fetch', (e) => {
+/*self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
+*/
